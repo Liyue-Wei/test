42dec3e937e26e966f4831baffb0bb11d4c9c318
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-19 15:40:58 +0800

    api key

--- ai_hello_world.py
+++ ai_hello_world.py
@@ -1,11 +1,11 @@
 import openai
 import os
 
-openai.api_key = "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"
-lang = ['python', 'c', 'c++', 'vb', 'swift', 'java', 'js', 'ruby', 'go', 'cs', 'r', 'powershell', 'lua', 'julia', 'PHP', 'Objective-C', 'vb.net7']
+openai.api_key = "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"
+lang = ['python', 'c', 'c++', 'vb', 'swift', 'java', 'js', 'ruby', 'go', 'cs', 'r', 'powershell', 'lua', 'julia', 'PHP', 'Objective-C', 'vb.net7', 'html']
 
 for i in range(0, len(lang)):
-    prompt = ("write a code to print hello world uaing " + lang[i])
+    prompt = ("write a code to print a heart with * uaing " + lang[i])
     completions = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=1280)
     message = completions.choices[0].text
     print(lang[i]+message+'\n')
