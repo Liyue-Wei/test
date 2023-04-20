ecee66edb58bbdae537f1cf6926a8af9c933d27e
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-20 11:35:39 +0800

    ai stabilization detection

--- ai_hello_world.py
+++ ai_hello_world.py
@@ -1,13 +1,24 @@
 import openai
 import os
+import time
 
-openai.api_key = "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"
-lang = ['python', 'c', 'c++', 'vb', 'swift', 'java', 'js', 'ruby', 'go', 'cs', 'r', 'powershell', 'lua', 'julia', 'PHP', 'Objective-C', 'vb.net7', 'html']
+time_now = time.time()
 
-for i in range(0, len(lang)):
-    prompt = ("write a code to print a heart with * uaing " + lang[i])
-    completions = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=1280)
-    message = completions.choices[0].text
-    print(lang[i]+message+'\n')
+openai.api_key = "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"
+lang = ['python', 'c', 'c++', 'vb', 'swift', 'java', 'js', 'ruby', 'go', 'cs', 'r', 'powershell', 'lua', 'julia', 'PHP', 'Objective-C', 'vb.net7', 'html', 'coldfusion', 'cython', 'cpython', 'fortran', 'kotlin', 'matlab']
+
+i = len(lang)
+print(i)
+
+while(i>0):
+    for n in range(0, len(lang)):
+        prompt = ("write a code to print hello world uaing " + lang[n])
+        completions = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1280)
+        message = completions.choices[0].textv
+        print(lang[n]+message+'\n')
+    i-=1
+    print('\n')
+    time_now = (time.time()-time_now)
+    print("time spend:", time_now)
 
 os.system('pause')
