1b9675d1d6d3a5ad25dbff0261d5b9d2d098e0c2
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-30 11:22:30 +0800

    daily update

--- ai_hello_world.py
+++ ai_hello_world.py
@@ -2,23 +2,23 @@ import openai
 import os
 import time
 
-time_now = time.time()
-
-openai.api_key = "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"
+openai.api_key = "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"
 lang = ['python', 'c', 'c++', 'vb', 'swift', 'java', 'js', 'ruby', 'go', 'cs', 'r', 'powershell', 'lua', 'julia', 'PHP', 'Objective-C', 'vb.net7', 'html', 'coldfusion', 'cython', 'cpython', 'fortran', 'kotlin', 'matlab']
 
 i = len(lang)
 print(i)
+f = open("output.txt", "w")
 
 while(i>0):
+    time_now = time.time()
     for n in range(0, len(lang)):
         prompt = ("write a code to print hello world uaing " + lang[n])
-        completions = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=1280)
+        completions = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1280)
         message = completions.choices[0].text
-        print(lang[n]+message+'\n')
-    i-=1
-    print('\n')
+        print(lang[n]+message+'\n', file=f)
+
+    i-=i
     time_now = (time.time()-time_now)
-    print("time spend:", time_now)
+    print('\n', "time spend:", time_now)
 
 os.system('pause')
