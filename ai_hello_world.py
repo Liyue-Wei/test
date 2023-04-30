e484589269e37953329e51938e805cabb305a67d
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-04-30 10:34:55 +0800

    api key

--- ai_hello_world.py
+++ ai_hello_world.py
@@ -4,7 +4,7 @@ import time
 
 time_now = time.time()
 
-openai.api_key = "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"
+openai.api_key = "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"
 lang = ['python', 'c', 'c++', 'vb', 'swift', 'java', 'js', 'ruby', 'go', 'cs', 'r', 'powershell', 'lua', 'julia', 'PHP', 'Objective-C', 'vb.net7', 'html', 'coldfusion', 'cython', 'cpython', 'fortran', 'kotlin', 'matlab']
 
 i = len(lang)
@@ -13,8 +13,8 @@ print(i)
 while(i>0):
     for n in range(0, len(lang)):
         prompt = ("write a code to print hello world uaing " + lang[n])
-        completions = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1280)
-        message = completions.choices[0].textv
+        completions = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=1280)
+        message = completions.choices[0].text
         print(lang[n]+message+'\n')
     i-=1
     print('\n')
