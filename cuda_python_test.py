551d2e219b746bb80f3ae6e179ae94553c672d4e
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-17 20:26:33 +0800

    Update cuda_python_test.py

--- cuda_python_test.py
+++ cuda_python_test.py
@@ -4,15 +4,15 @@ import os
 import time
 
 t_now = float(time.time())
-for i in range(10000):
-    x = cp.arange(1000000).reshape(1000, 1000).astype('f')
+for i in range(100000):
+    x = cp.arange(100000000).reshape(10000, 10000).astype('f')
 print(x, x.sum(axis=1))
 print("count with cupy by cuda {}".format(float(time.time())-t_now))
 
-t_now = float(time.time())
-for i in range(10000):
-    x = np.arange(1000000).reshape(1000, 1000).astype('f')
-print(x, x.sum(axis=1))
-print("count with numpy by cpu {}".format(float(time.time())-t_now))
+# t_now = float(time.time())
+# for i in range(10000):
+#     x = np.arange(1000000).reshape(1000, 1000).astype('f')
+# print(x, x.sum(axis=1))
+# print("count with numpy by cpu {}".format(float(time.time())-t_now))
 
 os.system('pause')
