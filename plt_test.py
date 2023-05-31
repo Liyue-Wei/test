68803b5b7a49737f08f9689d4b20ac35ad841be3
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-05-31 12:08:34 +0800

    Update plt_test.py

--- plt_test.py
+++ plt_test.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 7, 250000)   
+x = np.linspace(0, 7, 16384)   
 y = np.sin(x)    
 yy = np.cos(x)
 yyy = np.tan(x)
+tol = 20
+yyy[yyy > tol] = np.nan
+yyy[yyy < -tol] = np.nan
 
 fig, ax = plt.subplots()
 
-ax.plot(x, y, linewidth=1.0)  
-ax.set(xlim=(0, 6.282), xticks=np.arange(0, 6.282), ylim=(-1, 1), yticks=np.arange(-1, 1))
+# ax.plot(x, y, linewidth=1.0)  
+# ax.set(xlim=(0, 6.282), xticks=np.arange(0, 6.282), ylim=(-1, 1), yticks=np.arange(-1, 1))
+# 
+# ax.plot(x, yy, linewidth=1.0)  
+# ax.set(xlim=(0, 6.282), xticks=np.arange(0, 6.282), ylim=(-1, 1), yticks=np.arange(-1, 1))
 
-ax.plot(x, yy, linewidth=1.0)  
-ax.set(xlim=(0, 6.282), xticks=np.arange(0, 6.282), ylim=(-1, 1), yticks=np.arange(-1, 1))
-
-# ax.plot(x, yyy, linewidth=1.0)  
-# ax.set(xlim=(0, 16), xticks=np.arange(0, 32), ylim=(-2, 2), yticks=np.arange(-2, 2))
+ax.plot(x, yyy, linewidth=1.0)  
+ax.set(xlim=(0, 6.282), xticks=np.arange(0, 6.282), ylim=(-20, 20), yticks=np.arange(-1, 1))
 
 plt.show()
 
