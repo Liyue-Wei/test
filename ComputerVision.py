45eaaa897fe7aa7af19e45f0825b0c4340f8aa59
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-07-13 14:29:04 +0800

    nb_test移除

--- ComputerVision.py
+++ ComputerVision.py
@@ -1,7 +1,7 @@
 import cv2
 cap = cv2.VideoCapture(0)                         # 讀取電腦攝影機鏡頭影像。
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))    # 取得影像寬度
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 取得影像高度
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))    # 取得影像寬度
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 取得影像高度
 # fourcc = cv2.VideoWriter_fourcc(*'WEBM')          # 設定影片的格式為 MJPG
 # out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width,  height))  # 產生空的影片
 if not cap.isOpened():
