7b9f1d475ab2afbf64e26dd6c1eb01aa57b20804
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2023-09-11 05:13:55 +0800

    tkinter奇怪錯誤

--- tk_text.py
+++ tk_text.py
@@ -6,11 +6,9 @@ def getTextInput():
     result=textExample.get("1.0","end")
     print(result)
 
-textExample=tk.Text(root, height=10)
-textExample.pack()
-btnRead=tk.Button(root, height=1, width=10, text="Read", 
-                    command=getTextInput)
+textExample=tk.Text(root, height=10).pack()
 
-btnRead.pack()
+btnRead=tk.Button(root, height=1, width=10, text="Read", command=getTextInput).pack()
+
 
 root.mainloop()
