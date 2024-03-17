0573e60c320fc6dd4ae70cf848920d11dc35e5c7
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-03-18 07:40:19 +0800

    test

---
+++ TK_ICO.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('ICO_TEST')  # 更改标题名字
+window.geometry('250x50')  
+window.iconbitmap('YTD.ico')   # 更改窗口图标
+window.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+.iconbitmap('YTD.ico')
